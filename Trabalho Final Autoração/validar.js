cd461eca583789135bf2b10cd60bd2fc6b2c9bc4
humano <33604687+humanoumano@users.noreply.github.com>
2017-11-27 18:20:44 -0200

    agilizando

---
+++ Trabalho Final Autoração/validar.js
@@ -0,0 +1,20 @@
+function validacao() {
+
+	if(document.form.senha.value=="" || document.form.nome.value==""){
+		
+		alert("Por favor, preencha o campos."); 
+		document.form.senha.focus();
+		document.form.nome.focus();
+		return false;
+	}
+	
+	
+	
+	
+	if(document.form.senha.value.length < 8){
+		alert("A senha deve conter no mínimo 6 caracteres");
+		document.form.senha.focus();
+		return false;
+	}
+	
+}
