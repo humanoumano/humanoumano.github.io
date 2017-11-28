be3895c74d9205fe41673af34216ced049586950
humano <33604687+humanoumano@users.noreply.github.com>
2017-11-28 01:00:16 -0200

    adiantando

--- Trabalho Final Autoração/javascript.js
+++ Trabalho Final Autoração/javascript.js
@@ -1,8 +1,6 @@
-
 var compradosLixeira = 0;
 var compradosLixo = 0;
 var compradosLuxo = 0;
-
 
 function validacao() {
 
@@ -25,15 +23,26 @@ function validacao() {
 	
 }
 
+setInterval(draw(), 200);
+
+function draw(){
+
  $( function() {
 				$( "#luxo" ).accordion();
 				$( "#entulho" ).accordion();
 				$( "#outrolixo" ).accordion();
 				
 				
+				$('#carroLuxo').text('Luxos acumulados: ' +compradosLuxo);
+				$('#carroLixeira').text('Lixeiras acumuladas: ' +compradosLixeira);
+				$('#carroLixo').text('Lixo acumulado: ' +compradosLixo);
+				
+				
+	
+				
 			  } );
 
-			  
+}
 function comprarLixeira(){
 	
 	compradosLixeira = compradosLixeira +1;
@@ -51,3 +60,16 @@ function comprarLuxo(){
 	compradosLuxo = compradosLuxo +1;
 	
 	}
+	
+function efetuarCompra(){
+	
+	 alert( "Parabéns você comprou"  );
+	 
+	 compradosLixeira = 10;
+	 compradosLixo = 10;
+	 compradosLuxo = 10;
+					
+	}
+
+	
+
