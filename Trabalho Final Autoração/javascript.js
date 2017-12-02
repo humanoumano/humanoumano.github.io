b22fb0fd06f5ac842f0ca61f586e1895df3cf91e
humano <33604687+humanoumano@users.noreply.github.com>
2017-12-02 13:33:33 -0200

    slider image

--- Trabalho Final Autoração/javascript.js
+++ Trabalho Final Autoração/javascript.js
@@ -48,7 +48,7 @@ function draw(){
 				$('#carroLixeira').text('Lixeiras acumuladas: ' +carrinhoLixeira);
 				$('#carroLixo').text('Lixo acumulado: ' +carrinhoLixo);
 				
-				
+				 
 	
 				
 			  } );
